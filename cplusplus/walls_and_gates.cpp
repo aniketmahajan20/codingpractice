7dcf9ca2869140a8119742e194d00aaeb9b76857
Aniket <aniketmahajan20@gmail.com>
2024-08-11 12:24:29 -0700

    working solution for wallsandgate problem

--- cplusplus/walls_and_gates.cpp
+++ cplusplus/walls_and_gates.cpp
@@ -1,30 +1,55 @@
 #include <vector>
 #include <queue>
+#include <set>
 #include <iostream>
+#include <cmath>
 
 using namespace std;
 
-#define INF     (2^31 - 1)
+#define INF     (pow(2,31) - 1)
+
+#define TARGET  0
+
+vector<int> directions = {-1,0,1,0,-1};
 
 class Solution {
+    int rows;
+    int columns;
 public:
     void wallsAndGates(vector<vector<int>>& rooms) {
-        int rows = rooms.size();
-        int columns = rooms[0].size();
-        cout << rows << columns;
+        queue<pair<int,int>> q;
+        rows = rooms.size();
+        columns = rooms[0].size();
+        for (int i = 0; i < rows; i++){
+            for (int j = 0; j < columns; j++){
+                if (rooms[i][j] == 0){
+                    q.emplace(i,j);
+                }
+            }
+        }
+        int step = 0;
+        while(!q.empty()){
+            ++step;
+            int size = q.size();
+            for (int i =0; i<size; i++){
+                pair<int,int> cur = q.front();
+
+                for (int k = 0; k < 4; k++){
+                    int row = cur.first + directions[k];
+                    int column = cur.second + directions[k+1];
+
+                    if (row >= 0 && column >=0 && row < rows && column < columns && rooms[row][column] == INF){
+                        rooms[row][column] = step;
+                        q.emplace(row,column);
+                    }
+                }
+                q.pop();
+            }
+        }
     }
 };
 
-
-int main(){
-    Solution* obj = new Solution();
-    vector<vector<int>> twoD_array{
-        {1,2},
-        {3,4}
-    };
-    for(int i=0; i<5; i++){
-
-    }
-    obj->wallsAndGates(twoD_array);
-    return 0;
-}
+// int main(){
+//     cout << INT_MAX << "   " << INF;
+//     return 0;
+// }
