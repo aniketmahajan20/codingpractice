467dd77e1ccce60cae563d0b375673cbe9197a2d
Aniket <aniketmahajan20@gmail.com>
2024-09-18 15:16:45 -0700

    added recursive solutions to merge two sorted lists problem

--- cplusplus/merge_two_sorted_list.cpp
+++ cplusplus/merge_two_sorted_list.cpp
@@ -32,6 +32,64 @@ public:
         }
         prev->next = (l1 == NULL)?l2:l1;
         return prehead.next;
+    }
+};
 
+class Solution_Recursion {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode prehead = ListNode(99);
+        ListNode* prev = &prehead;
+        merge_two_lists(l1,l2, prev);
+        return prehead.next;
+    }
+
+    void merge_two_lists(ListNode* l1, ListNode* l2, ListNode* prev) {
+        if (l1 == NULL) {
+            prev->next = l2;
+            return;
+        }
+        if (l2 == NULL) {
+            prev->next =  l1;
+            return;
+        }
+        if (l1->val < l2->val) {
+            prev->next = l1;
+            merge_two_lists(prev->next->next, l2, prev->next);
+        }
+        else {
+            prev->next = l2;
+            merge_two_lists(l1, prev->next->next, prev->next);
+        }
+    }
+};
+
+class Solution_Tail_Recursion {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode prehead = ListNode(99);
+        ListNode* prev = &prehead;
+        merge_two_lists(l1,l2, prev);
+        return prehead.next;
+    }
+
+    void merge_two_lists(ListNode* l1, ListNode* l2, ListNode* prev) {
+        if (l1 == NULL) {
+            prev->next = l2;
+            return;
+        }
+        if (l2 == NULL) {
+            prev->next =  l1;
+            return;
+        }
+        if (l1->val < l2->val) {
+            prev->next = l1;
+            l1 = l1->next;
+        }
+        else {
+            prev->next = l2;
+            l2 = l2->next;
+        }
+        merge_two_lists(l1, l2, prev->next);
     }
 };
