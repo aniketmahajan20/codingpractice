9e238ab9c633b4f80edd6d806aa26bf3aa893b94
Aniket <aniketmahajan20@gmail.com>
2024-10-30 14:32:41 -0700

    added solution for validate binary search tree problem

---
+++ cplusplus/validate_binary_search_tree.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+
+using namespace std;
+
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    bool isValidBST(TreeNode* root) {
+        return is_valid_bst(root, NULL, NULL);
+    }
+
+    bool is_valid_bst(TreeNode* root, TreeNode* min, TreeNode* max){
+        if (root == NULL){
+            return true;
+        }
+        if (min != NULL && root->val <= min->val){
+            return false;
+        }
+        if (max != NULL && root->val >= max->val){
+            return false;
+        }
+        return is_valid_bst(root->left, min, root) && is_valid_bst(root->right, root, max);
+    }
+};
+        
